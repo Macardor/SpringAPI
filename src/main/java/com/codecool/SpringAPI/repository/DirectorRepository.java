988390aba30db7061614277d13504795b074b446
Dominik <domek99@gmail.com>
2020-11-03 12:01:53 +0100

    add findByisActive method

--- src/main/java/com/codecool/SpringAPI/repository/DirectorRepository.java
+++ src/main/java/com/codecool/SpringAPI/repository/DirectorRepository.java
@@ -3,5 +3,9 @@ package com.codecool.SpringAPI.repository;
 import com.codecool.SpringAPI.model.Director;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DirectorRepository extends JpaRepository<Director, Long> {
+
+    List<Director> findByisActive(boolean isActive);
 }
