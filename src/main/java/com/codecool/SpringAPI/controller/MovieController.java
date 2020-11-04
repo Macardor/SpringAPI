ddc1aec6bfe9db926b90607b36b560183d7e74cd
Maciek <macardorpl@gmail.com>
2020-11-04 09:49:25 +0100

    Logger in MovieController

--- src/main/java/com/codecool/SpringAPI/controller/MovieController.java
+++ src/main/java/com/codecool/SpringAPI/controller/MovieController.java
@@ -1,9 +1,10 @@
 package com.codecool.SpringAPI.controller;
 
-import com.codecool.SpringAPI.SpringApiApplication;
 import com.codecool.SpringAPI.exception.MovieNotFoundException;
 import com.codecool.SpringAPI.model.Movie;
 import com.codecool.SpringAPI.repository.MovieRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @RestController
 public class MovieController {
     private final MovieRepository repository;
+    private final Logger log = LogManager.getLogger(MovieRepository.class);
 
     MovieController(MovieRepository repository) {
         this.repository = repository;
@@ -18,27 +20,28 @@ public class MovieController {
 
     @GetMapping("/movies")
     public List<Movie> all() {
+        log.info("Getting all movies");
         List<Movie> movies = repository.findAll();
         movies.removeIf(movie -> !movie.isActive());
-        SpringApiApplication.logger.info("Got all movies");
         return movies;
     }
 
     @PostMapping("/movies")
     public Movie newMovie(@RequestBody Movie newMovie) {
+        log.info("Adding new movie: " + newMovie.getTitle());
         return repository.save(newMovie);
     }
 
-    // Single item
-
     @GetMapping("/movies/{id}")
     public Movie one(@PathVariable Long id) {
+        log.info("Getting movie id: " + id);
         return repository.findById(id)
                 .orElseThrow(() -> new MovieNotFoundException(id));
     }
 
     @PutMapping("/movies/{id}")
     public Movie replaceActor(@RequestBody Movie newMovie, @PathVariable Long id) {
+        log.info("Putt on movie id: " + id);
         return repository.findById(id)
                 .map(movie -> {
                     movie.setTitle(newMovie.getTitle());
@@ -55,6 +58,7 @@ public class MovieController {
 
     @DeleteMapping("/movies/{id}")
     void deleteMovie(@PathVariable Long id) {
+        log.info("Deleting movie id: " + id);
         repository.getOne(id).setActive(false);
     }
 }
