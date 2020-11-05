b0a1962bdc05d9ab78aed7b5b13c8fb7ab19a229
Dominik <domek99@gmail.com>
2020-11-05 11:50:17 +0100

    delete logger inside class to use aop logger

--- src/main/java/com/codecool/SpringAPI/controller/MovieController.java
+++ src/main/java/com/codecool/SpringAPI/controller/MovieController.java
@@ -1,18 +1,16 @@
 package com.codecool.SpringAPI.controller;
 
 import com.codecool.SpringAPI.model.Movie;
-import com.codecool.SpringAPI.repository.MovieRepository;
 import com.codecool.SpringAPI.service.MovieService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Component
 public class MovieController {
     private final MovieService movieService;
-    private final Logger log = LogManager.getLogger(MovieRepository.class);
 
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
@@ -20,31 +18,26 @@ public class MovieController {
 
     @GetMapping("/movies")
     public List<Movie> all(@RequestParam(value = "title", defaultValue = "") String title) {
-        log.info("Getting all movies");
         return movieService.getAllMovies(title);
     }
 
     @PostMapping("/movies")
     public Movie newMovie(@RequestBody Movie newMovie) {
-        log.info("Adding new movie: " + newMovie.getTitle());
         return movieService.addMovie(newMovie);
     }
 
     @GetMapping("/movies/{id}")
     public Movie one(@PathVariable Long id) {
-        log.info("Getting movie id: " + id);
         return movieService.getMovie(id);
     }
 
     @PutMapping("/movies/{id}")
     public Movie replaceActor(@RequestBody Movie newMovie, @PathVariable Long id) {
-        log.info("Put on movie id: " + id);
         return movieService.updateMovie(newMovie, id);
     }
 
     @DeleteMapping("/movies/{id}")
     void deleteMovie(@PathVariable Long id) {
-        log.info("Deleting movie id: " + id);
         movieService.deleteMovie(id);
     }
 }
